67b6bab040a7b4edf2c30bd6b9f51ee495af5ee1
Pengfei Xu <pengfei.xu@intel.com>
2022-02-02 21:28:43 +0800

    xstate.c: separate xrstor, fork and xsave steps
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- x86/xstate.c
+++ x86/xstate.c
@@ -41,6 +41,7 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sched.h>
+#include <stdbool.h>
 #include <sys/wait.h>
 #include <sys/syscall.h>
 
@@ -51,33 +52,40 @@
 struct xsave_buffer *xstate_buf0, *xstate_buf1, *xstate_buf2, *xstate_buf3;
 static uint32_t xstate_size;
 static uint64_t xsave_test_mask;
-unsigned long long child_pid;
+static pid_t process_pid;
 
 void free(void *ptr);
-static void test_xstate_buffer()
+static inline bool __validate_xstate_regs(struct xsave_buffer *buf0)
 {
-	printf("[RUN]\tStashed xstate buffer should same as restored xstate.\n");
-	set_xstate_data(xstate_buf0, xsave_test_mask);
-	dump_buffer((unsigned char *)xstate_buf0, xstate_size);
+	int ret;
 
-	xrstor(xstate_buf0, xsave_test_mask);
+	xrstor(buf0, xsave_test_mask);
 	xsave(xstate_buf1, xsave_test_mask);
-	if (compare_buf((unsigned char *)xstate_buf0,
-			(unsigned char *)xstate_buf1, xstate_size))
-		printf("[FAIL]\tStashed xstate is not same as restored xstate.\n");
-	else
-		printf("[PASS]\tStashed xstate is same as restored xstate.\n");
+	ret = memcmp(&buf0->bytes[0], &xstate_buf1->bytes[0], xstate_size);
+	memset(xstate_buf1, 0, xstate_size);
+
+	if (ret == 0)
+		return false;
+	return true;
+}
+
+static inline void validate_xstate_regs_same(struct xsave_buffer *buf)
+{
+	int ret = __validate_xstate_regs(buf);
+
+	if (ret != 0)
+		fatal_error("Xstate registers changed");
 }
 
 static void usr1_pollute_xstate_handler(int signum, siginfo_t *info,
 	void *__ctxp)
 {
 	if (signum == SIGUSR1) {
-		set_xstate_data(xstate_buf2, xsave_test_mask);
+		set_rand_xstate_data(xstate_buf2, xsave_test_mask);
 		/* Xstate of SIGUSR1 handling is not affected by SIGUSR2 handling */
 		xsave_syscall_test((unsigned char *)xstate_buf2,
 			(unsigned char *)xstate_buf3, xsave_test_mask, (int)SYS_kill,
-			child_pid, SIGUSR2, 0, 0, 0, 0);
+			process_pid, SIGUSR2, 0, 0, 0, 0);
 
 		if (compare_buf((unsigned char *)xstate_buf2,
 			(unsigned char *)xstate_buf3, xstate_size))
@@ -91,43 +99,37 @@ static void usr2_nested_pollute_xstate_handler(int signum, siginfo_t *info,
 	void *__ctxp)
 {
 	if (signum == SIGUSR2) {
-		set_xstate_data(xstate_buf3, xsave_test_mask);
+		set_rand_xstate_data(xstate_buf3, xsave_test_mask);
 		xrstor(xstate_buf3, xstate_size);
 	}
 }
 
-static int test_xstate_sig_hanlde(void)
+static int test_xstate_sig_handle(void)
 {
-	pid_t child;
-	int status, cycle_num;
+	int cycle_num, fail_num = 0;
 
-	memset(xstate_buf1, 0, xstate_size);
-	/* Use child process testing to avoid exceptions blocking the next test */
-	child = fork();
-	if (child < 0)
-		fatal_error("Create child pid failed");
-	else if	(child == 0) {
-		printf("[RUN]\tCheck xstate around signal handling test.\n");
-		child_pid = getpid();
+	sethandler(SIGUSR1, usr1_pollute_xstate_handler, 0);
+	sethandler(SIGUSR2, usr2_nested_pollute_xstate_handler, 0);
+	printf("[RUN]\tCheck xstate around signal handling test.\n");
+	set_rand_xstate_data(xstate_buf0, xsave_test_mask);
+	validate_xstate_regs_same(xstate_buf0);
 
-		for (cycle_num = 1; cycle_num <= CYCLE_MAX_NUM; cycle_num++) {
-			xsave_syscall_test((unsigned char *)xstate_buf0,
-				(unsigned char *)xstate_buf1, xsave_test_mask, (int)SYS_kill,
-				child_pid, SIGUSR1, 0, 0, 0, 0);
-			if (compare_buf((unsigned char *)xstate_buf0,
-					(unsigned char *)xstate_buf1, xstate_size)) {
-				printf("[FAIL]\tProcess xstate is not same after signal handling\n");
-				_exit(0);
-			}
+	process_pid = getpid();
+	for (cycle_num = 1; cycle_num <= CYCLE_MAX_NUM; cycle_num++) {
+		xsave_syscall_test((unsigned char *)xstate_buf0,
+			(unsigned char *)xstate_buf1, xsave_test_mask, (int)SYS_kill,
+			process_pid, SIGUSR1, 0, 0, 0, 0);
+		if (compare_buf((unsigned char *)xstate_buf0,
+				(unsigned char *)xstate_buf1, xstate_size)) {
+			fail_num++;
+			printf("[FAIL]\tProcess xstate is not same after signal handling\n");
+			break;
 		}
+	}
+	if(fail_num == 0)
 		printf("[PASS]\tProcess xstate is same after signal handling.\n");
-		_exit(0);
-	}
-
-	if (child) {
-		if (waitpid(child, &status, 0) != child || !WIFEXITED(status))
-			fatal_error("Child quit unexpectedly\n");
-	}
+	clearhandler(SIGUSR1);
+	clearhandler(SIGUSR2);
 
 	return 0;
 }
@@ -136,27 +138,28 @@ static int test_process_switch(void)
 {
 	pid_t grandchild;
 	int status;
+	unsigned char *buf0 = (unsigned char *)xstate_buf0;
+	unsigned char *buf3 = (unsigned char *)xstate_buf3;
+	uint64_t xsave_mask = xsave_test_mask;
 
+	xrstor((struct xsave_buffer *)buf0, xsave_mask);
 	/* Child process performs process switching by forking grandchild process */
-	grandchild = xsave_syscall_test((unsigned char *)xstate_buf0,
-			(unsigned char *)xstate_buf3, xsave_test_mask,
-			(int)SYS_fork, 0, 0, 0, 0, 0, 0);
+	grandchild = __fork();
 	if (grandchild < 0)
 		fatal_error("grandchild fork failed\n");
-	if (grandchild == 0) {
+	else if (grandchild == 0) {
 		/* fork syscall succeeded, now in the grandchild */
 		printf("\tGrandchild pid:%d changed it's own xstates\n", getpid());
-		set_xstate_data(xstate_buf2, xsave_test_mask);
+		set_rand_xstate_data(xstate_buf2, xsave_test_mask);
 		xrstor(xstate_buf2, xsave_test_mask);
 		_exit(0);
-	}
-	if (grandchild > 0) {
+	} else if (grandchild > 0) {
 		/* fork syscall succeeded, still in the first child. */
 		if (waitpid(grandchild, &status, 0) != grandchild ||
 			!WIFEXITED(status))
 			fatal_error("Grandchild exit with error status");
 		else {
-			//xsave(xstate_buf3, xsave_test_mask);
+			xsave((struct xsave_buffer *)buf3, xsave_mask);
 			printf("\tChild:%d check xstate with mask:0x%lx after process switching\n",
 				getpid(), xsave_test_mask);
 			if (compare_buf((unsigned char *)xstate_buf0,
@@ -217,6 +220,14 @@ static int test_xstate_fork(void)
 	return 0;
 }
 
+static void init_xstate_buf(void)
+{
+	xstate_buf0 = alloc_xbuf(xstate_size);
+	xstate_buf1 = alloc_xbuf(xstate_size);
+	xstate_buf2 = alloc_xbuf(xstate_size);
+	xstate_buf3 = alloc_xbuf(xstate_size);
+}
+
 static void free_xstate_buf(void)
 {
 	free(xstate_buf0);
@@ -232,20 +243,13 @@ int main(void)
 	xstate_size = get_xstate_size();
 	/* Check CPU capability by CPU id and set CPU capability flags */
 	xsave_test_mask = check_cpuid_xstate();
-	xstate_buf0 = alloc_xbuf(xstate_size);
-	xstate_buf1 = alloc_xbuf(xstate_size);
-	xstate_buf2 = alloc_xbuf(xstate_size);
-	xstate_buf3 = alloc_xbuf(xstate_size);
-	test_xstate_buffer();
 
-	sethandler(SIGUSR1, usr1_pollute_xstate_handler, 0);
-	sethandler(SIGUSR2, usr2_nested_pollute_xstate_handler, 0);
-	test_xstate_sig_hanlde();
+	init_xstate_buf();
+
+	test_xstate_sig_handle();
 
 	test_xstate_fork();
 
-	clearhandler(SIGUSR1);
-	clearhandler(SIGUSR2);
 	free_xstate_buf();
 
 	return 0;
